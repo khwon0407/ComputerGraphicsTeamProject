19cbae6ffbe3aacfdaed5f14f95ffe1cce0828ad
caydesun1024 <miniandghksl@pusan.ac.kr>
2024-12-25 10:28:21 +0900

    BGM, primitive mesh 배경 및 버그 수정.

--- GraphicsTeamProj/Assets/script/MyMeshFilter.cs
+++ GraphicsTeamProj/Assets/script/MyMeshFilter.cs
@@ -1,43 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
- [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
- public class MyMeshFilter : MonoBehaviour
-
- {
-    private MeshFilter meshFilter;
-    private Vector3[] vertices;
-     private int[] indices;
-    private Vector2[] uv;
-
-    private void Awake()
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class MyMeshFilter : MonoBehaviour
+{
+    private void Start()
     {  
-        meshFilter = GetComponent<MeshFilter>();
-        GenerateMesh();
-    }  
-    private void GenerateMesh(){
-        vertices = new Vector3[3]
+        GenerateMesh(); // Unity Editor에서 설정한 MeshFilter에 Mesh를 생성합니다.
+    }
+
+    private void GenerateMesh()
+    {
+        // 16:9 비율로 정점 정의
+        Vector3[] vertices = new Vector3[4]
         {
-            new Vector3(0, 0, 0),
-            new Vector3(0, 1, 0),
-            new Vector3(1, 0, 0)
+            new Vector3(-16, -9, 0), // Bottom-left
+            new Vector3(-16,  9, 0), // Top-left
+            new Vector3( 16,  9, 0), // Top-right
+            new Vector3( 16, -9, 0)  // Bottom-right
         };
-        indices = new int[3] { 0, 1, 2 };
-        uv = new Vector2[3]
+
+        // 두 개의 삼각형으로 메시 구성
+        int[] indices = new int[6] { 0, 1, 2, 0, 2, 3 };
+
+        // UV 좌표 (텍스처 매핑 지원)
+        Vector2[] uv = new Vector2[4]
         {
             new Vector2(0, 0),
             new Vector2(0, 1),
+            new Vector2(1, 1),
             new Vector2(1, 0)
         };
-        
-        Mesh mesh = new Mesh();
-        mesh.name = "test1";
-        mesh.vertices = vertices;
-        mesh.triangles= indices;
+
+        // 메시 생성 및 설정
+        Mesh mesh = new Mesh
+        {
+            name = "BackgroundQuad",
+            vertices = vertices,
+            triangles = indices,
+            uv = uv
+        };
+
         mesh.RecalculateBounds();
         mesh.RecalculateNormals();
-        meshFilter.mesh = mesh;
+
+        GetComponent<MeshFilter>().mesh = mesh; // Unity Editor에서 설정한 MeshFilter에 할당
     }
 }
-
